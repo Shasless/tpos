ca759ed05d18a204980e4043db474f7ef1e6d12f
Shasless <54817015+Shasless@users.noreply.github.com>
2021-05-28 10:56:25 +0200

    Update tpex2.c

--- tpex2.c
+++ tpex2.c
@@ -1,33 +1,45 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<pthread.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <ctype.h>
 
 
-void *threadrun(void *a){
-        printf("hello I'm %d",getpid());
-	
+void *threadrun(void *arg)
+{
+    printf("Hello i'm %d\n", getpid());
+    return NULL;
 }
 
-int main(int argc, const char* argv[]){
+int main(int argc, char *argv[])
+{
+    int nb;
+    pthread_t *threads;
 
-        int nb = atoi(argv[1]);
-        pthread_t threads;
-	threads =malloc(nb * sizeof(pthread_t));
+    if (argc != 2){
+        perror("Need number of thread arg");
+        return -1;}
+    else
+    {
+        nb = atoi(argv[1]);
+    }
 
-	if(argc =!2){
-		printf("erreur");
-		exit(0);
-	}
-       
+    if ((threads = calloc(nb, sizeof(pthread_t))) == NULL)
+        exit(EXIT_FAILURE);
 
-        for( int y; y < nb; y++){
-                pthread_create(&threads[y], NULL, threadrun, NULL);
-        }
-        for(int y; y < nb; y++){
-                pthread_join(threads[y], NULL);
-        }
-        printf("-------THE END--------");
-        free(threads);
-        return 0;
+    for (int i = 0; i < nb; i++)
+    {
+      pthread_create(&(threads[i]), NULL,&threadrun, NULL);
+    }
+
+
+    for (int i = 0; i < nb; i++)
+    {
+        pthread_join(threads[i], NULL);
+            
+    }
+
+    free(threads);
+    exit(EXIT_SUCCESS);
 }
