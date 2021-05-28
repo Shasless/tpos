64eab01ccc64a38a691a0c83157fec2b5da222d8
Shasless <54817015+Shasless@users.noreply.github.com>
2021-05-28 11:04:40 +0200

    Update tpex4.c

--- tpex4.c
+++ tpex4.c
@@ -1,42 +1,66 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<pthread.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <ctype.h>
 
-int Somme = 0;
-pthread_mutex_t lock;
+ 
+int Somme = 0; 
+pthread_mutex_t mutexT;
 
+void *threadrun(void *a)
+{
+    printf("Hello i'm %d, et i'm from %d\n", getpid(),*(int *) a );
+    
+     for (int j = 0; j < 1000000; j++) { 
+        pthread_mutex_lock(&mutexT); 
+        Somme += *(int *) a; 
+        pthread_mutex_unlock(&mutexT); 
+    } 
+ 
 
-void *threadrun(void *a){
-        printf("hello I'm %d, and i'm of the thread number %d",getpid(),*(int *) a);
-
-	for (int j = 0; j< 1000000; j++){
-		pthread_mutex_lock(&lock);
-		Somme += *(int *) a;
-		pthread_mutex_unlock(&lock);
-	}
-
-	return NULL;
+    return NULL;
 }
 
-int main(int argc, const char* argv[]){
+int main(int argc, char *argv[])
+{
+    int nb;
+    pthread_t *threads;
+    int *args = calloc(sizeof(int), nb);
 
-        int nb = atoi(argv[1]);
-        pthread_t threads[nb];
-	int args[nb];
-       
-	
-	if(pthread_mutex_init(&lock, NULL) !=0){
-		printf("eror in the mutex");
-		return 1;
-	}
-        for( int y; y < nb; y++){
-		args[y] = y;
-                pthread_create(&threads[y], NULL, threadrun, &args[y]);
-        }
-        for(int y; y < nb; y++){
-                pthread_join(threads[y], NULL);
-        }
-        printf("-------THE END--------");
-        return 0;
+    if (argc != 2){
+        perror("Need number of thread arg");
+        return -1;}
+    else
+    {
+        nb = atoi(argv[1]);
+    }
+    
+    if (pthread_mutex_init(&mutexT, NULL) != 0) { 
+        printf("\n mutex init failed\n"); 
+        return -1; 
+    }
+    if ((threads = calloc(nb, sizeof(pthread_t))) == NULL)
+        exit(EXIT_FAILURE);
+
+    for (int i = 0; i < nb; i++)
+    {
+      args[i] = i;
+      pthread_create(&(threads[i]), NULL,&threadrun, &args[i]);
+    }
+
+
+    for (int i = 0; i < nb; i++)
+    {
+        pthread_join(threads[i], NULL);
+            
+    }
+    
+    printf("Somme: %d \n", Somme);
+    printf("------------THE END------------");
+    free(threads);
+    
+    return 0;
+    
 }
